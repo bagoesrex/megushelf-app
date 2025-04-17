5305c0c8314141f88da19e83839b391760c4ab93
bagoesrex <bagoesrex@gmail.com>
2025-04-17 23:18:39 +0700

    feat: set up express server and connect to mongodb

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import { dbConfig } from "./config/db.js";
+
+const app = express();
+const PORT = 5000;
+
+mongoose
+  .connect(dbConfig.uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("Error connecting to MongoDB:", err));
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
