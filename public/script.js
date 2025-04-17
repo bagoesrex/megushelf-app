0244f06f87dea7e58f09ae52163b4bcdb2ef9679
bagoesrex <bagoesrex@gmail.com>
2025-04-17 20:15:43 +0700

    feat: implement loadcontent function for switching main content

---
+++ public/script.js
@@ -0,0 +1,21 @@
+function loadContent(section, el) {
+  const content = document.getElementById("main-content");
+  document
+    .querySelectorAll(".sidebar .nav-item")
+    .forEach((item) => item.classList.remove("active"));
+  el.classList.add("active");
+
+  if (section === "dashboard") {
+    content.innerHTML = `
+          <h2>Dashboard</h2>
+        `;
+  } else if (section === "category") {
+    content.innerHTML = `
+          <h2>Category Management</h2>
+        `;
+  } else if (section === "books") {
+    content.innerHTML = `
+          <h2>Book Management</h2>
+        `;
+  }
+}
