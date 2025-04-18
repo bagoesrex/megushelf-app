a2a47b3ccbf12d88b06cf817f0c95eb9897bc32d
bagoesrex <bagoesrex@gmail.com>
2025-04-18 11:20:46 +0700

    chore: update deployment config

--- config/db.js
+++
@@ -1,3 +0,0 @@
-export const dbConfig = {
-  uri: "mongodb+srv://bagoesrex:sikucink@cluster0.io5olsm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-};
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
-import { dbConfig } from "./config/db.js";
 import dotenv from "dotenv";
 
 dotenv.config();
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+console.log("Mongo URI: ", process.env.MONGO_URI);
 
 import bookRoutes from "./routes/bookRoutes.js";
 import bookCategoryRoutes from "./routes/bookCategoryRoutes.js";
@@ -13,14 +14,14 @@ const app = express();
 const PORT = 3000;
 
 mongoose
-  .connect(dbConfig.uri)
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Connected to MongoDB");
     app.listen(PORT, () => {
       console.log(`Server running on http://localhost:${PORT}`);
     });
   })
-  .catch((err) => console.error("MongoDB connection error:", err));
+  .catch((err) => {console.error("MongoDB connection error:", err)});
 
 app.use(cors());
 app.use(express.json());
