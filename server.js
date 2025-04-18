ddaca1bfa9e241b7395a0ad213236841bb1721b1
bagoesrex <bagoesrex@gmail.com>
2025-04-18 12:27:36 +0700

    feat: add dynamic path resolution for serving static files

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 import dotenv from "dotenv";
-
 dotenv.config();
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 console.log("Mongo URI: ", process.env.MONGO_URI);
 
@@ -13,6 +17,12 @@ import bookCategoryRoutes from "./routes/bookCategoryRoutes.js";
 const app = express();
 const PORT = 3000;
 
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
@@ -21,14 +31,12 @@ mongoose
       console.log(`Server running on http://localhost:${PORT}`);
     });
   })
-  .catch((err) => {console.error("MongoDB connection error:", err)});
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api", bookRoutes);
 app.use("/api", bookCategoryRoutes);
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
